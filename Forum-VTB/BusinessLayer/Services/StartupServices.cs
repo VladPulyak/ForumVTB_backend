d12786fdf52a1da8f7c57d3cd13a8eb7e4899323
VladPulyak <vlad.pulyak03@mail.ru>
2023-08-04 08:53:59 +0300

    Added seed data for subsections

--- Forum-VTB/BusinessLayer/Services/StartupServices.cs
+++ Forum-VTB/BusinessLayer/Services/StartupServices.cs
@@ -16,7 +16,13 @@ namespace BusinessLayer.Services
                 dbContext.Database.Migrate();
                 await RolesSeedDataAsync(dbContext);
                 await SectionsSeedDataAsync(dbContext);
-                await SubsectionsSeedDataAsync(dbContext);
+                await TransportSubsectionsSeedDataAsync(dbContext);
+                await ElectronicsSubsectionsSeedDataAsync(dbContext);
+                await SportsAndRelaxSubsectionsSeedDataAsync(dbContext);
+                await AnimalsSubsectionsSeedDataAsync(dbContext);
+                await RealEstateSubsectionsSeedDataAsync(dbContext);
+                await ClothesAndPersonalThingsSubsectionsSeedDataAsync(dbContext);
+                await HouseAndGardenSubsectionsSeedDataAsync(dbContext);
             }
         }
 
@@ -53,9 +59,9 @@ namespace BusinessLayer.Services
             }
         }
 
-        private static async Task SubsectionsSeedDataAsync(ForumVTBDbContext dbContext)
+        private static async Task TransportSubsectionsSeedDataAsync(ForumVTBDbContext dbContext)
         {
-            if (!await dbContext.Subsections.AnyAsync())
+            if (!await dbContext.Subsections.AnyAsync(q => q.SectionId == 1))
             {
                 await dbContext.Subsections.AddRangeAsync(new List<Subsection>
                 {
@@ -71,9 +77,104 @@ namespace BusinessLayer.Services
             }
         }
 
-        public static async Task PrintConnectionString(string connectionString)
+        private static async Task ElectronicsSubsectionsSeedDataAsync(ForumVTBDbContext dbContext)
         {
-            await Console.Out.WriteLineAsync(connectionString);
+            if (!await dbContext.Subsections.AnyAsync(q => q.SectionId == 2))
+            {
+                await dbContext.Subsections.AddRangeAsync(new List<Subsection>
+                {
+                    new Subsection {Name = "Phones and accessories", SectionId = 2},
+                    new Subsection {Name = "Computers and accessories", SectionId = 2},
+                    new Subsection {Name = "Audio and video", SectionId = 2},
+                    new Subsection {Name = "Laptops", SectionId = 2},
+                    new Subsection {Name = "Phototechnics", SectionId = 2},
+                    new Subsection {Name = "Tablets, e-books", SectionId = 2},
+                    new Subsection {Name = "Office equipment", SectionId = 2}
+                });
+
+                await dbContext.SaveChangesAsync();
+            }
+        }
+
+        private static async Task SportsAndRelaxSubsectionsSeedDataAsync(ForumVTBDbContext dbContext)
+        {
+            if (!await dbContext.Subsections.AnyAsync(q => q.SectionId == 3))
+            {
+                await dbContext.Subsections.AddRangeAsync(new List<Subsection>
+                {
+                    new Subsection {Name = "Bicycles and accessories", SectionId = 3},
+                    new Subsection {Name = "Hunting and fishing", SectionId = 3},
+                    new Subsection {Name = "Tourism", SectionId = 3},
+                    new Subsection {Name = "Sport", SectionId = 3}
+                });
+
+                await dbContext.SaveChangesAsync();
+            }
+        }
+
+        private static async Task AnimalsSubsectionsSeedDataAsync(ForumVTBDbContext dbContext)
+        {
+            if (!await dbContext.Subsections.AnyAsync(q => q.SectionId == 4))
+            {
+                await dbContext.Subsections.AddRangeAsync(new List<Subsection>
+                {
+                    new Subsection {Name = "Dogs", SectionId = 4},
+                    new Subsection {Name = "Cats", SectionId = 4},
+                    new Subsection {Name = "Birds", SectionId = 4},
+                    new Subsection {Name = "Aquarium", SectionId = 4},
+                    new Subsection {Name = "Other", SectionId = 4}
+                });
+
+                await dbContext.SaveChangesAsync();
+            }
+        }
+
+        private static async Task RealEstateSubsectionsSeedDataAsync(ForumVTBDbContext dbContext)
+        {
+            if (!await dbContext.Subsections.AnyAsync(q => q.SectionId == 5))
+            {
+                await dbContext.Subsections.AddRangeAsync(new List<Subsection>
+                {
+                    new Subsection {Name = "Selling", SectionId = 5},
+                    new Subsection {Name = "Daily rent", SectionId = 5},
+                    new Subsection {Name = "Long-term rental", SectionId = 5},
+                    new Subsection {Name = "Commercial estate", SectionId = 5}
+                });
+
+                await dbContext.SaveChangesAsync();
+            }
+        }
+
+        private static async Task ClothesAndPersonalThingsSubsectionsSeedDataAsync(ForumVTBDbContext dbContext)
+        {
+            if (!await dbContext.Subsections.AnyAsync(q => q.SectionId == 6))
+            {
+                await dbContext.Subsections.AddRangeAsync(new List<Subsection>
+                {
+                    new Subsection {Name = "Clothing, shoes, accessories", SectionId = 6},
+                    new Subsection {Name = "Products for children", SectionId = 6},
+                    new Subsection {Name = "Beauty and health", SectionId = 6}
+                });
+
+                await dbContext.SaveChangesAsync();
+            }
+        }
+
+        private static async Task HouseAndGardenSubsectionsSeedDataAsync(ForumVTBDbContext dbContext)
+        {
+            if (!await dbContext.Subsections.AnyAsync(q => q.SectionId == 7))
+            {
+                await dbContext.Subsections.AddRangeAsync(new List<Subsection>
+                {
+                    new Subsection {Name = "Repair and construction", SectionId = 7},
+                    new Subsection {Name = "Furniture and interior", SectionId = 7},
+                    new Subsection {Name = "Appliances", SectionId = 7},
+                    new Subsection {Name = "Plants", SectionId = 7},
+                    new Subsection {Name = "Cookware and kitchen goods", SectionId = 7}
+                });
+
+                await dbContext.SaveChangesAsync();
+            }
         }
     }
 }
